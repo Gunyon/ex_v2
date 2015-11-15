e2abf55b9b34fb6d389336c1749248f929dc915a
Gunyon <gabureac.oleg@gmail.com>
2015-11-15 17:18:31 +0200

    Upload on github
    Upload on github

---
+++ allvalute.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Curs valutar</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+		<script type="text/javascript" src="js/allvalute.js"></script>
+	</head>
+	<body onload="process()">
+		<table class="maintable" align="center" width="600" border="0" cellspacing="0">
+			<tr>
+				<td align="center" colspan="2">
+					<h1 class="toptext">Curs valutar Banca Naţională a Moldovei</h1>
+				</td>
+			</tr>
+			<tr>
+				<?php include "blocks/topNav.php" ?>
+			</tr>
+			<tr valign="top">
+				<td align="center" id="allcourses">
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">&nbsp;</td>
+			</tr>
+		</table>
+	</body>
+</html>
---
+++ news.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Stiri</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body onload="process()">
+		<table class="maintable" align="center" border="0" cellspacing="0">
+			<tr>
+				<td align="center" colspan="2">
+					<h1 class="toptext">Curs valutar Banca Naţională a Moldovei</h1>
+				</td>
+			</tr>
+			<tr>
+				<?php include "blocks/topNav.php" ?>
+			</tr>
+			<tr valign="top">
+				<td>
+					<h4 class="news">
+					<img class="ico_news" src="img/ico_news.png" />
+					<a href="news/news1.html">Guvernatorul BNM participă la aniversarea Băncii Naţionale a României</a>
+					</h4>
+					<h4 class="news">
+					<img class="ico_news" src="img/ico_news.png" />
+					<a href="news/news2.html">O conferinţă de presă cu tema: "Raportul de politică monetară" la Banca Naţională a Moldovei</a>
+					</h4>
+					<h4 class="news">
+					<img class="ico_news" src="img/ico_news.png" />
+					<a href="news/news3.html">Prezentarea Guvernatorului BNM Dorin Drăguţanu în cadrul săptămânii investiţionale "Moldova Business Week 2010"</a>
+					</h4>
+					<h4 class="news">
+					<img class="ico_news" src="img/ico_news.png" />
+					<a href="news/news4.html">Seminarul cu tema: "Instruire privind gestionarea riscurilor şi prevenirea delictelor în sectorul bancar"</a>
+					</h4>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">&nbsp;</td>
+			</tr>
+		</table>
+	</body>
+</html>
